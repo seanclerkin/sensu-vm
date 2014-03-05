f20843aeb0b3dadcc428bad7e7ac1511882a1592
seanclerkin <gingernator007@googlemail.com>
2014-03-05 21:29:54 +0000

    Added an unless to the rabbitmq install

--- puppet/modules/sensu/manifests/rabbitmq/install.pp
+++ puppet/modules/sensu/manifests/rabbitmq/install.pp
@@ -2,6 +2,7 @@ class sensu::rabbitmq::install {
 	exec { "wget_rabbitmq_rpm":
     command => "wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.2.4/rabbitmq-server-3.2.4-1.noarch.rpm && yum -y install rabbitmq-server-3.2.4-1.noarch.rpm",
     path => "/usr/bin:/usr/sbin:/bin:/usr/local/bin",
+    unless => "rpm -qa | grep rabbitmq-server"
     cwd => "/tmp"
   }
 }
